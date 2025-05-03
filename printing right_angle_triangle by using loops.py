12db17ac72e76d26be79ae48ea5b66efba3d1411
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-03 05:38:21 -0800

    Delete printing right_angle_triangle by using loops.py

--- printing right_angle_triangle by using loops.py
+++
@@ -1,9 +0,0 @@
-def print_right_angle_triangle(rows):  # defining a function to print a right angle triangle
-    for i in range(1, rows + 1):   # using for loop to iterate over the element
-        # in the outer loop 1 is the starting value of row and "row" variable holds the last value of the loop
-        # and loop variable "i" hold the current value of the loop; exp: in first iteration it's value is 1 "i=1"
-        # during 2nd "i = 2" and during 3rd "i = 3" and so on
-        # point to be notice {I am using outer loop here just to iterate over the elements of row in vertical manner}
-        # so the outer for loop will execute until its value will be equal to the value, user will enter.
-        
-
