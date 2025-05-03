482f5c0756eca6960ad4eaa020d55f2e9e4a09f6
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-03 06:02:11 -0800

    Update printing right_angle_triangle by using loops.py

--- printing right_angle_triangle by using loops.py
+++ printing right_angle_triangle by using loops.py
@@ -7,3 +7,11 @@ for i in range(1, rows + 1):  # using for loop to iterate over the element
 # during 2nd "i = 2" and during 3rd "i = 3" and so on
 # # point to be notice {I am using outer loop here just to iterate over the elements of row in vertical manner}
 # so the outer for loop will execute until its value will be equal to the value, user will enter.
+    for j in range(1, i + 1):  # then I am using inner loop to iterate over the elements of outer for loop(from 1
+    # to current value of i)
+    # j will hold the current value of "i", the variable of outer loop
+    # this nested loop will print "*" this character in columns in horizontal manner.
+    # Let's say; "i = 1" then j will hold the current value of i and " j = 1 " and one "*" character will be
+    # printed.
+    # "i = 2" and "j = 2" two "*" characters will be printed
+    # "i = 3" and "j = 3" three "*" characters will be printed and so on
