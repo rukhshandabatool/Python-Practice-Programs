0e67e72d00156666bad553f287b8f72a467f21f0
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-04 06:34:02 -0800

    Update Area_of_circle_function.py

--- Area_of_circle_function.py
+++ Area_of_circle_function.py
@@ -8,11 +8,3 @@ def circle_area(radius):  # defining a function to calculate the area of circle
 r = float(input("Enter the radius of circle:"))  # getting input from user with the help of input() and float() functions
 Calculated_Area = circle_area(r)   # calling function with input variable as argument for passing the value to function
 print("{} is the calculated area of circle.".format(Calculated_Area))  # Printing area of circle by using format() function
-
-
-
-
-
-    
-
-
