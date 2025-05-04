3f94177190ad2119168664d472b6dc4ec53a572f
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-04 06:10:36 -0800

    Update Area_of_circle_function.py

--- Area_of_circle_function.py
+++ Area_of_circle_function.py
@@ -9,7 +9,7 @@ r = float(input("Enter the radius of circle:"))  # getting input from user with 
 # some languages) is used in circumference calculations because the result is likely to be a decimal number. The circumference formula 
 # involves pi (π), which is an irrational number (a number with infinitely non-repeating decimal digits), and often the radius itself might
 # also be a decimal. To ensure accurate results, variables used in the calculation (like radius, pi, and the resulting circumference) should
-# be able to hold decimal values, which float (or double) provides
+# be able to hold decimal values, which float (or double) provides.
 
 Calculated_Area = circle_area(r)   # calling function with input variable as argument for passing the value to function
 print("{} is the calculated area of circle.".format(Calculated_Area))  # Printing area of circle by using format() function
