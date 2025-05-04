caff23530cb1306f730f47a6db2e28a4ff3de613
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-04 05:52:20 -0800

    Area_Circumference_of_circle_function.py

--- Area_Circumference_of_circle_function.py
+++ Area_Circumference_of_circle_function.py
@@ -1,29 +1,9 @@
-def circle_area(radius):  # defining a function ro calculate the area of circle
-    pi = 3.14  # initialize predefined value of pi
-    return pi * radius ** 2   # returning area by using formula
+# Different ways to calculate the circumference of a circle by using functions
+
+# Method 1:
+def circle_area(radius):  # defining a function to calculate the area of circle
 
 
-r = float(input("Enter the radius of circle:"))  # getting input from user
-Calculated_Area = circle_area(r)   # calling function with input variable as argument
-print("{} is the calculated area of circle.".format(Calculated_Area))  # Printing area of circle
+    
 
 
-def circle_circumference(radius):  # defining a function to calculate the circumference of circle
-    pi = 3.14  # initializing pi variable with predefined value
-    return 2 * pi * radius  # returning circumference by using formula
-
-
-r = float(input("Enter radius of circle:"))
-Calculated_circumference = circle_circumference(r)
-print("{} is the calculated circumference of circdef circle_area(radius))  # defining a function ro calculate the area of circle
-    pi = 3.14  # initialize predefined value of pi
-    return pi * radius ** 2   # returning area by using formula
-
-
-r = float(input("Enter the radius of circle:"))  # getting input from user
-Calculated_Area = circle_area(r)   # calling function with input variable as argument
-print("{} is the calculated area of circle.".format(Calculated_Area))  # Printing area of circle
-
-
-def circle_circumference(radius):  # defining a function to calculate the circumference of circle
-    pi = 3.14  # initialle.".format(Calculated_circumference))
