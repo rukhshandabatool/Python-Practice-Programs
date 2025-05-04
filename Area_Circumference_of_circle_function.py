1cd96c2a98cef0ce947dbc1ed4d8cf8c38205cf2
RukhshandaBatool0 <rukhshandabatool0@gmail.com>
2025-05-04 05:53:28 -0800

    Update Area_Circumference_of_circle_function.py

--- Area_Circumference_of_circle_function.py
+++ Area_Circumference_of_circle_function.py
@@ -2,7 +2,8 @@
 
 # Method 1:
 def circle_area(radius):  # defining a function to calculate the area of circle
-
+    pi = 3.14  # initialize predefined value of pi
+    return pi * radius ** 2   # returning area by using predefined mathematical formula 
 
     
 
